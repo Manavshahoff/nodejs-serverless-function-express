f3c18f92afcabc7e149f2d5f96bd0b8e1c803819
Manavshahoff <manavshah.off@gmail.com>
2024-08-23 14:57:03 -0400

    add tsconfig

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { User, Group, Expense } = require("./Mongo");
+const {User, Group, Expense} = require('./Mongo');
 const app = express();
 
 app.use(express.json());
